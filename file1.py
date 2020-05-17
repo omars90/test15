9e35e570a964491b533b91a4c4f941e3198b48a8
omars90 <omar.alsulamy@gmail.com>
2020-05-17 05:30:04 +0300

    Create file1.py

---
+++ file1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+// input height in metrics
+height = float(input('Enter your height: '))
+if 260>height>100:
+    height=height*0.01
+elif 0.8>height>8.53:
+    height=height*30.48
+print(height)
