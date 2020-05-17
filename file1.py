f0ae04ccd3f4f44b5dff213b03aca469121a9f61
omars90 <omar.alsulamy@gmail.com>
2020-05-17 06:47:16 +0300

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-// input height in metrics
+#input height in metrics
 height = float(input('Enter your height: '))
 if 260>height>100:
     height=height*0.01
-elif 0.8>height>8.53:
+elif 0.8<height<8.53:
     height=height*30.48
+
+height=round(height,2)
 print(height)
